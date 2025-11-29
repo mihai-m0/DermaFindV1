11adead96066bf6324f0647fca3504343c230eb7
mihai-m0 <mihaimorarp@gmail.com>
2025-11-29 15:57:40 +0200

    Added deployment files (requirements and Procfile), updated API path.

--- site/script.js
+++ site/script.js
@@ -1,7 +1,7 @@
 // =======================================================
 // CONFIGURARE ȘI CONSTANTE
 // =======================================================
-const API_URL = "http://127.0.0.1:5000/api/predict";
+const API_URL = "/api/predict";
 const PRIMARY_COLOR = "#7B1F45";
 
 // Obținem referințele la elementele HTML
